7e8a706fbd6b802ec768cb4911fba1a38ce0a3c5
Pell <S03027105@ccc.ccofc.edu>
2023-09-05 12:40:09 -0600

    Date and song

---
+++ Date/src/Date.java
@@ -0,0 +1,14 @@
+
+public class Date {
+
+	public static void main(String[] args) {
+		// What is todays date?
+		int date = 5;
+		String day = "Tuesday";
+		String month = "September";
+		int year = 2023;
+		System.out.println("Todays date is" + " " + day + "," + date + " " + month +"," + year);
+
+	}
+
+}
