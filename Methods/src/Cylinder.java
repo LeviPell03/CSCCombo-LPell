b91b7908e4dfac962ed166bc95f82850d8b10893
Pell <S03027105@ccc.ccofc.edu>
2023-09-19 12:36:47 -0600

    Methods.

--- Methods/src/Cylinder.java
+++ Methods/src/Cylinder.java
@@ -3,26 +3,31 @@ import java.util.Scanner;
 public class Cylinder {
 
 	public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
-		
-	 
-	final double PI = 3.141592653;
-	
-	
-	System.out.println("Please enter radius of the circle");
-	int r = input.nextInt();
-	
-	int d = 2 * r;
-	double circumference = 2 * PI * r;
-	
-	double area = PI * r * r;
-	
-	System.out.println("Diameter of circle is " + d);
-	System.out.println("Circumference of the circle is " + circumference);
-	System.out.println("Area of circle is " + area);
-	
-	
-	
+		Scanner input = new Scanner(System.in);
+
+		final double PI = 3.141592653;
+
+		System.out.println("Please enter radius of the circle");
+		int radius = input.nextInt();
+
+		int d = 2 * radius;
+		double circumference = 2 * PI * radius;
+
+		double area = PI * radius * radius;
+
+		System.out.println("Diameter of circle is " + d);
+		System.out.println("Circumference of the circle is " + circumference);
+		System.out.println("Area of circle is " + area);
+
+	}
+
+	public static double computeArea(double radius) {
+		return PI * radius * radius;
+	}
+
+	public static double computeVolume(double area) {
+		return 
+
 	}
 
 }
