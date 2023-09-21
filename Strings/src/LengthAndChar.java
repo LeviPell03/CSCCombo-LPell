d966dcdf231a31790191f7e302321daeeb4bc47a
Pell <S03027105@ccc.ccofc.edu>
2023-09-21 12:30:01 -0600

    String

---
+++ Strings/src/LengthAndChar.java
@@ -0,0 +1,17 @@
+
+public class LengthAndChar {
+
+	public static void main(String[] args) {
+		// Length and Char
+		String fruit = "Apple";
+		int len = fruit.length();
+		char letter = fruit.charAt(len - 1);
+		System.out.println(letter);
+		
+		String empty = "";
+		String str = empty + "How are you? /n";
+		
+		System.out.println(str);
+	}
+
+}
