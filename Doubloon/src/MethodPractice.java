5efaff46a50c9cc5c49ae9e79abc8a3a7baa55e9
Pell <S03027105@ccc.ccofc.edu>
2023-09-25 12:44:46 -0600

    Doubloon

--- Doubloon/src/MethodPractice.java
+++ Doubloon/src/MethodPractice.java
@@ -1,9 +1,26 @@
+import java.util.Scanner;
 
 public class MethodPractice {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		boolean doubloon = isDoubloo;
+		System.out.println("Please enter in a word");
+		if (doubloon) {
+			System.out.println("The word is indeed a doubloon");
+		}
+		
+}
 
-	}
-
+	public static boolean isDoubloon(String word1) {
+		String word = "Abba";
+		for (int i = 0; i < word.length(); i++) {
+			char letter = word.charAt(0);
+			int len = word.length();
+			int count = 0;
+			for (intj = 0; i < String.length(); i++) {
+			
+			
+			
+			
 }
