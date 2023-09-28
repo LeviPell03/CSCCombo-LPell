f65501d3c0ebae0e370af3ac15754c85ebfd2e2a
Wonch <Robert.Wonch@frontrange.edu>
2023-09-28 11:29:24 -0600

    test

---
+++ Exam1/src/PrintMessage.java
@@ -0,0 +1,9 @@
+
+public class PrintMessage {
+
+	public static void main(String[] args) {
+		// First programming test
+		System.out.println("This is my first programming test");
+	}
+
+}
---
+++ Exam1/src/PrintMessage1.java
@@ -0,0 +1,9 @@
+
+public class PrintMessage1 {
+
+	public static void main(String[] args) {
+		System.out.println("This is my first programming test");
+
+	}
+
+}
