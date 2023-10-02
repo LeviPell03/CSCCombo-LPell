00674f7c5701592eb65d05edde986f4bb73c4ad7
Pell <S03027105@ccc.ccofc.edu>
2023-10-02 12:42:04 -0600

    Array

---
+++ Arrays/src/Array.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Array {
+
+	public static void main(String[] args) {
+		// Array
+		int[] arr = {3,6,4,6};
+		int total = 0;
+		for (int i = 0; i < arr.length; i++) {
+			total += arr.length;
+			// Average is "+="
+			// Making something green is CTRL + 7
+		}
+		System.out.println("The average is " + total);
+		
+	}
+
+}
