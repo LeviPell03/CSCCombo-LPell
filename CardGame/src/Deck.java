3d78412b635a738ff89e5334e8a74ec523c978f0
Pell <S03027105@ccc.ccofc.edu>
2023-10-27 12:38:49 -0600

    Shuffle

--- CardGame/src/Deck.java
+++ CardGame/src/Deck.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.Random;
+
 
 public class Deck {
 	private Card[] cards;
@@ -26,14 +28,29 @@ public class Deck {
 	public void setCards(Card[] cards) {
 		this.cards = cards;
 	}
-
+	
+	public void Shuffle() {
+		Random rand = new Random();
+		
+		for (int i = 0; i < cards.length; i++) {
+			int index = rand.nextInt(cards.length);
+			 Card tempCard = cards[i];
+			 cards[i] = cards[index];
+			 cards[index] = tempCard;
+			 
+			 
+		}
+	}
+	
 	@Override
 	public String toString() {
-		String retStr = "";
+		StringBuilder retStr = new StringBuilder();
 		for ( int i = 0; i < cards.length; i++) {
-			retStr = retStr + cards[i].toString() + " \n ";
+			retStr.append(cards[i].toString());
+			retStr.append("\n");
 		}
-		return retStr;
+		
+		return retStr.toString();
 	}
 	
 }
--- CardGame/src/War.java
+++ CardGame/src/War.java
@@ -2,10 +2,13 @@
 public class War {
 
 	public static void main(String[] args) {
-		//Card card1 = new Card(11,3);
-	//	System.out.println(card1);
+		// Card card1 = new Card(11,3);
+	    // System.out.println(card1);
 		
 		Deck deck = new Deck();
+		System.out.println(deck);
+		
+		deck.Shuffle();
 		System.out.println(deck);
 
 	}
