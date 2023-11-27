c6bf96ed434ee961b32f084b5f883e2e71c1878a
Pell <S03027105@ccc.ccofc.edu>
2023-11-27 12:40:14 -0700

    FileWriter

---
+++ FileIO/src/FileWrite.java
@@ -0,0 +1,28 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+// import java.io.FileOutputStream;
+import java.io.*;
+public class FileWrite {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		File myFile = new File("C:\\Users\\S03027105\\testwritehome.txt");
+		if (!myFile.exists()) {
+			if (myFile.createNewFile() ) {
+				System.out.println("New file created" +myFile.getName());
+			}
+		}
+		
+		// PrintWriter printer = new PrintWriter(myFile.getName());
+		PrintWriter printer = new PrintWriter(new FileOutputStream(myFile.getName(), false));
+		printer.println("The moon is full today");
+		printer.println("I'm excited!");
+		printer.println("It's an auspicious day!");
+		
+		printer.flush();
+		printer.close();
+
+	}
+
+}
