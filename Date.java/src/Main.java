ddcc28f7de215d79068a4f7925af6317fbf01772
Levi <KiyonoriFox@Gmail.Com>
2024-02-01 17:10:48 -0700

    TemperatureConversion

--- Date.java/src/Main.java
+++ Date.java/src/Main.java
@@ -2,26 +2,24 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-            // I am going to tell Java to print "Hello World!" to the console.
-            System.out.println("\n");
-            System.out.println("Hello World!");
-            System.out.println("\n");
+        System.out.println("\n");
+        System.out.println("Hello World!");
+        System.out.println("\n");
 
-            // The date printed to the console
-            int day = 1;
-            int month = 24;
-            int year = 2024;
-            System.out.println(day + "/" + month + "/" + year);
-            System.out.println("\n");
-            System.out.println(day  + "\n" + month + "\n" + year);
+        // The date printed to the console
+        int day = 1;
+        int month = 24;
+        int year = 2024;
+        System.out.println(day + "/" + month + "/" + year);
+        System.out.println("\n");
+        System.out.println(day  + "\n" + month + "\n" + year);
 
-            // The date in 2nd formatting printed to the console
-            String day1 = "Wednesday";
-            int month1 = 24;
-            int year2 = 2024;
-            System.out.println("\n");
-            System.out.println("Today's date is " + day1 + "," + " " + month1 + "," + " " + year2 + "." + " It's a whole new year, I can't believe it.");
-
+        // The date in 2nd formatting printed to the console
+        String day1 = "Wednesday";
+        int month1 = 24;
+        int year2 = 2024;
+        System.out.println("\n");
+        System.out.println("Today's date is " + day1 + "," + " " + month1 + "," + " " + year2 + "." + " It's a whole new year, I can't believe it.");
 
         }
     }
