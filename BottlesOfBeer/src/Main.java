4815f86b5711005111e13627d38c8e635d9fee29
Levi <KiyonoriFox@Gmail.Com>
2024-02-18 02:55:23 -0700

    BottlesOfBeer

--- BottlesOfBeer/src/Main.java
+++ BottlesOfBeer/src/Main.java
@@ -1,39 +1,48 @@
 import java.util.Scanner;
 
+import static java.lang.System.in;
+
 public class Main {
     public static void main(String[] args) {
-
+        // Imported a Scanner and made a new Scanner.
         Scanner in = new Scanner(System.in);
-        // Imported a Scanner from Java's utility. Made a new Scanner by telling Java to create one.
-        // Initialized my int as "beer".
-        int beer;
-        // By default, word will be "bottles"
         String word = "bottles";
-        // System.out asking the user to enter a number.
-        System.out.println("What's your starting number of bottles?");
-        // The user's input will be printed to the console next.
-        beer = in.nextInt();
-        // while beer is greater than 0 these if loops will continue until you reach 0 bottles of beer.
-        while (beer > 0) {
-            // When you have one bottle left, "bottles" becomes "bottle"
-            if (beer == 1) {
-                word = "bottle";
-            }
-            // System.out with the addition of when beer is used in this loop, subtract 1.
-            System.out.println(beer + " " + word + " of beer on the wall, " + beer + " " + word + " of beer.");
-            beer = beer - 1;
-            // if the beer is greater than 0, the System.out will  say the lyrics to this part of the song.
-            if (beer > 0) {
-                System.out.println("Take one down, pass it round " + beer + " " + word + " of beer.");
-            }
-        }
-        // When the beer reaches 0 the song ends.
-        if (beer == 0) {
-            System.out.println("No more bottles of beer.");
-        }
+        // Asking for an input from the user.
+        System.out.println("Hello! Enter number of beer. Thank you. ");
+        // Input from the user will be the next thing printed to the console.
+        int beer = in.nextInt();
 
+        // for loop saying if i = beer, beer greater than or equal to 0, deduct. It has a nested if loop. when beer is greater than 1 do this task.
+        for (int i = beer; i >= 0; i--) {
+            if (i > 1) {
+                System.out.println(i + " bottles of beer on the wall, " + i + " " + word + " of beer.");
+                System.out.println("Take one down and pass it around, " + i + " " + word + " of beer on the wall.");
+                // else if beer is equal to 1 do this.
+            }  if (i == 1) {
+                word = "bottle";
+
+                System.out.println(i + " bottle of beer on the wall, " + i + " " + word + " of beer.");
+                System.out.println("Take one down and pass it around, " + i + " " + word + " of beer on the wall.");
+                // else if beer is equal to 0, do this.
+            } else if (i == 0) {
+                System.out.println("No more bottles of beer on the wall, no more bottles of beer.");
+                // This is tricky. I have to use "beer" here because if I use "i" it will be 0.
+                System.out.println("Go to the store and buy some more, " + beer + " bottles of beer on the wall.");
+            }
+        }
     }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
 
